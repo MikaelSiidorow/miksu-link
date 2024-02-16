0f235271dc99b56255d9da252e108ff7392a8b0a
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-02-16 20:59:44 +0200

    add download button to qr-code image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,6 +44,33 @@
 		{#if codeData}
 			<div role="alert" class="alert alert-success w-full max-w-md">
 				<img src={codeData} alt="QR Code" />
+				<div>
+					<button
+						class="btn btn-square"
+						type="button"
+						on:click={() => {
+							if (!codeData) return;
+
+							const a = document.createElement('a');
+							a.href = codeData;
+							a.download = 'qr-code.png';
+							document.body.appendChild(a);
+							a.click();
+							document.body.removeChild(a);
+						}}
+					>
+						<span class="sr-only">Download QR code</span>
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24"
+							><path
+								fill="currentColor"
+								d="M4.75 17.25a.75.75 0 0 1 .75.75v2.25c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V18a.75.75 0 0 1 1.5 0v2.25A1.75 1.75 0 0 1 18.25 22H5.75A1.75 1.75 0 0 1 4 20.25V18a.75.75 0 0 1 .75-.75"
+							/><path
+								fill="currentColor"
+								d="M5.22 9.97a.749.749 0 0 1 1.06 0l4.97 4.969V2.75a.75.75 0 0 1 1.5 0v12.189l4.97-4.969a.749.749 0 1 1 1.06 1.06l-6.25 6.25a.749.749 0 0 1-1.06 0l-6.25-6.25a.749.749 0 0 1 0-1.06"
+							/></svg
+						>
+					</button>
+				</div>
 			</div>
 		{/if}
 
@@ -78,14 +105,13 @@
 					>
 						<span class="sr-only">Copy to clipboard</span>
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24"
-							><g fill="none" fill-rule="evenodd"
-								><path
-									d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
-								/><path
-									fill="currentColor"
-									d="M9 2a2 2 0 0 0-2 2v2h2V4h11v11h-2v2h2a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM4 7a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zm0 2h11v11H4z"
-								/></g
-							></svg
+							><path
+								fill="currentColor"
+								d="M7.024 3.75c0-.966.784-1.75 1.75-1.75H20.25c.966 0 1.75.784 1.75 1.75v11.498a1.75 1.75 0 0 1-1.75 1.75H8.774a1.75 1.75 0 0 1-1.75-1.75Zm1.75-.25a.25.25 0 0 0-.25.25v11.498c0 .139.112.25.25.25H20.25a.25.25 0 0 0 .25-.25V3.75a.25.25 0 0 0-.25-.25Z"
+							/><path
+								fill="currentColor"
+								d="M1.995 10.749a1.75 1.75 0 0 1 1.75-1.751H5.25a.75.75 0 1 1 0 1.5H3.745a.25.25 0 0 0-.25.25L3.5 20.25c0 .138.111.25.25.25h9.5a.25.25 0 0 0 .25-.25v-1.51a.75.75 0 1 1 1.5 0v1.51A1.75 1.75 0 0 1 13.25 22h-9.5A1.75 1.75 0 0 1 2 20.25z"
+							/></svg
 						>
 					</button>
 				</div>
